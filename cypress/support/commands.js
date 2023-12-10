f2359e0b4a14da226ea4cce5ab14c2b653a4f273
deboraroliveira <deborardsoliveira@gmail.com>
2023-12-09 23:19:08 -0300

    creating fillMandatoryFieldsAndSubmit customized command

---
+++ cypress/support/commands.js
@@ -0,0 +1,8 @@
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
+    const shortText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+    cy.get('#firstName').type('Debora')
+    cy.get('#lastName').type('Oliveira')
+    cy.get('#email').type('debora.oliveira@gmail.com')
+    cy.get('#open-text-area').type(shortText, {delay:0})
+    cy.contains('button', 'Enviar').click()
+})
