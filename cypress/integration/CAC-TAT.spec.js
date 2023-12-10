cc530616dcf3b5456d84e5e58bae5751081f3826
deboraroliveira <deborardsoliveira@gmail.com>
2023-12-09 23:15:14 -0300

    adding 'preenche os campos obrigatórios e envia o formulário' test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,4 +9,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+    
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        const longText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+        cy.get('input[id = "firstName"]').type('Debora')
+        cy.get('input[id = "lastName"]').type('Oliveira')
+        cy.get('input[id = "email"]').type('debora.oliveira@gmail.com')
+        cy.get('textarea[id = "open-text-area"]').type(longText, {delay:0})
+        cy.contains('button', 'Enviar').click()
+        cy.get('.success').should('be.visible')
+    })
 })
