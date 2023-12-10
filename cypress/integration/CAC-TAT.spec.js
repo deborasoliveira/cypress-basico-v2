6f0d1247afc4240a3a418a160f6de8dac23e4057
deboraroliveira <deborardsoliveira@gmail.com>
2023-12-09 23:17:27 -0300

    adding 'exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios' test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -76,4 +76,10 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .clear()
         .should('have.value', '')
     })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.contains('button', 'Enviar').click()
+        cy.get('.error').should('be.visible')
+    })
+
 })
