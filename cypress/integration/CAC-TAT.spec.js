7b316cf40b16fd48987bc7bd98e12fb3c1e1605d
deborasoliveira <debora.raquel710@gmail.com>
2023-12-14 23:38:09 -0300

    adding 'seleciona um arquivo simulando um drag-and-drop' test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -138,4 +138,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             expect($input[0].files[0].name).to.equal('example.json')
         } )
     })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function(){
+
+        cy.get('input[type="file"]#file-upload')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
 })
