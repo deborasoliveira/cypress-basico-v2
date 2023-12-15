a407efb700cc24221105bc2134bb7ee4e33bef09
deborasoliveira <debora.raquel710@gmail.com>
2023-12-14 23:37:37 -0300

    adding 'seleciona um arquivo da pasta fixtures' test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -130,4 +130,12 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('be.not.checked')
     })
 
+    it('seleciona um arquivo da pasta fixtures', function(){
+        cy.get('input[type="file"]#file-upload')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json')
+        .should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        } )
+    })
 })
