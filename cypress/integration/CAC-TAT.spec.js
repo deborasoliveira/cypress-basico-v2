25e6ccc6b910445aa5b712f2fda1f946612056cf
deborasoliveira <debora.raquel710@gmail.com>
2023-12-27 22:53:41 -0300

    adding 'exibe e esconde as mensagens de sucesso e erro usando o .invoke()' test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -188,4 +188,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         
         cy.contains('CAC TAT - Política de privacidade').should('be.visible')
     })
+
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke()', function(){
+        cy.get('.success')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Mensagem enviada com sucesso.')
+        .invoke('hide')
+        .should('not.be.visible')
+
+        cy.get('.error')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Valide os campos obrigatórios!')
+        .invoke('hide')
+        .should('not.be.visible')
+    })
 })
