5754b4f5ea718b30f20e28e25c23f0f3cbc341d6
deborasoliveira <debora.raquel710@gmail.com>
2023-12-27 22:50:24 -0300

    adding loadash _.times function to 'testa a página da política de privacidade de forma independente' test

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,6 @@
-it('testa a página da política de privacidade de forma independente', function(){
-    cy.visit('./src/privacy.html')
-    .contains('CAC TAT - Política de privacidade')
+Cypress._.times(3, function(){
+    it('testa a página da política de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html')
+        .contains('CAC TAT - Política de privacidade')
+    })
 })
